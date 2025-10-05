be98868ef234118806f4fb6c54a11f819e145637
pawel-misztal <mrpawolo@gmail.com>
2025-10-05 10:55:13 +0200

    rm dist

--- README.md
+++
@@ -1,1 +0,0 @@
-# pawel-misztal.github.io
--- src/components/HoleGradientTransition.tsx
+++
@@ -1,3 +0,0 @@
-export function HoleGradientTransition() {
-  return <div className="fixed bottom-0 w-full h-40 bg-gradient-to-t from-zinc-900 to-transparent -z-10"></div>;
-}
